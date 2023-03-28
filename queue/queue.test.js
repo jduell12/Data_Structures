5912140c326d05425eea7d7f1d8abf66d4fe79f6
Jessica Duell <jessica.duell@pendo.io>
2023-03-28 13:09:30 -0400

    update test

--- queue/queue.test.js
+++ queue/queue.test.js
@@ -8,17 +8,17 @@ describe('Queue', ()=>{
     })
     
     it('length returns 0 for empty queue', ()=>{
-        expect(q.length).toEqual(0)
+        expect(q.length()).toEqual(0)
     })
     
     it('returns correct length after enqueue', ()=>{
-        expect(q.length).toEqual(0)
+        expect(q.length()).toEqual(0)
         
         q.enqueue(2)
-        expect(q.length).toEqual(1)
+        expect(q.length()).toEqual(1)
         
         q.enqueue(4)
-        expect(q.length).toEqual(2)
+        expect(q.length()).toEqual(2)
         
         q.enqueue(6)
         q.enqueue(8)
@@ -27,12 +27,12 @@ describe('Queue', ()=>{
         q.enqueue(14)
         q.enqueue(16)
         q.enqueue(18)
-        expect(q.length).toEqual(9)
+        expect(q.length()).toEqual(9)
     })
     
     it('dequeue empty queue', ()=>{
         expect(q.dequeue()).toBeNull()
-        expect(q.length).toEqual(0)
+        expect(q.length()).toEqual(0)
     })
     
     it('dequeue respects order', ()=>{
@@ -41,12 +41,12 @@ describe('Queue', ()=>{
         q.enqueue(105)
         
         expect(q.dequeue()).toEqual(100)
-        expect(q.length).toEqual(2)
+        expect(q.length()).toEqual(2)
         
         expect(q.dequeue()).toEqual(101)
-        expect(q.length).toEqual(1)
+        expect(q.length()).toEqual(1)
         
         expect(q.dequeue()).toEqual(105)
-        expect(q.length).toEqual(0)
+        expect(q.length()).toEqual(0)
     })
 })
